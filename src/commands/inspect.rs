2c7fc1e1a81d5b7417c68b2f635ad8c0f94e5107
cqb13 <cqb13.main@gmail.com>
2025-04-13 22:15:06 -0400

    moved file loading and path validation to command mod.rs

--- src/commands/inspect.rs
+++ src/commands/inspect.rs
@@ -1,10 +1,7 @@
+use super::{load_file, validate_path};
 use crate::{
     display::board::{print_board, print_board_with_numbers},
-    prints, Error,
-};
-use std::{
-    io::Read,
-    path::{Path, PathBuf},
+    prints,
 };
 use tgg::{Game, GameData, TggFile};
 
@@ -71,38 +68,4 @@ pub fn inspect(raw_path: String) {
             std::process::exit(1);
         }
     }
-}
-
-fn load_file(path: PathBuf) -> Result<Vec<u8>, Error> {
-    let mut file = match std::fs::File::open(path) {
-        Ok(file) => file,
-        Err(err) => return Err(Error::IO(err)),
-    };
-
-    let mut buffer = Vec::new();
-
-    match file.read_to_end(&mut buffer) {
-        Ok(_) => {}
-        Err(err) => return Err(Error::IO(err)),
-    }
-
-    Ok(buffer)
-}
-
-fn validate_path(raw_path: String) -> Result<PathBuf, Error> {
-    let path = Path::new(&raw_path).to_owned();
-
-    if path.extension().and_then(|ext| ext.to_str()) != Some("tgg") {
-        return Err(Error::InvalidInputFileExtension);
-    }
-
-    if path.is_dir() {
-        return Err(Error::InputPathLeadsToDirectory);
-    }
-
-    if !path.exists() {
-        return Err(Error::InputFileDoesNotExist);
-    }
-
-    return Ok(path);
 }
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,2 +1,40 @@
+use crate::Error;
+use std::io::Read;
+use std::path::{Path, PathBuf};
+
 pub mod create;
 pub mod inspect;
+
+fn load_file(path: PathBuf) -> Result<Vec<u8>, Error> {
+    let mut file = match std::fs::File::open(path) {
+        Ok(file) => file,
+        Err(err) => return Err(Error::IO(err)),
+    };
+
+    let mut buffer = Vec::new();
+
+    match file.read_to_end(&mut buffer) {
+        Ok(_) => {}
+        Err(err) => return Err(Error::IO(err)),
+    }
+
+    Ok(buffer)
+}
+
+fn validate_path(raw_path: String) -> Result<PathBuf, Error> {
+    let path = Path::new(&raw_path).to_owned();
+
+    if path.extension().and_then(|ext| ext.to_str()) != Some("tgg") {
+        return Err(Error::InvalidInputFileExtension);
+    }
+
+    if path.is_dir() {
+        return Err(Error::InputPathLeadsToDirectory);
+    }
+
+    if !path.exists() {
+        return Err(Error::InputFileDoesNotExist);
+    }
+
+    return Ok(path);
+}
